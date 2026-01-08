ee2e0d5c9044c3845fc7bc3289a28c5ce5d26778
Dávid Miklós <david.miklos.work@gmail.com>
2026-01-08 11:38:41 +0200

    feat: add diffview.nvim for git diff and file history

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,22 @@
 -- Git integration
 return {
+  -- Diff viewer for git
+  {
+    'sindrets/diffview.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff view' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it file [H]istory' },
+      { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = '[G]it repo [H]istory' },
+      { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[G]it diff [Q]uit' },
+      { '<leader>gm', '<cmd>DiffviewOpen origin/main...HEAD<cr>', desc = '[G]it diff vs [M]ain' },
+      { '<leader>gs', '<cmd>DiffviewOpen --staged<cr>', desc = '[G]it diff [S]taged' },
+    },
+    opts = {
+      use_icons = false,
+    },
+  },
+
   -- Git conflict markers resolution
   {
     'akinsho/git-conflict.nvim',
